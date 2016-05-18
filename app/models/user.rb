0f36ee38192bcbfab4e168ba3afd7bb1f1f13d4d
Kiko Marrs <findkiko@gmail.com>
2016-05-18 01:00:29 -0700

    Make user dropdown full name labeled

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :invoices
   has_many :issued_invoices, :class_name => 'Invoice', :foreign_key => 'issuer_id'
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
