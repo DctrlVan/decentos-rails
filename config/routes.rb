69b716ae51f41048106c711cfd8177ff07d9a540
Kiko Marrs <findkiko@gmail.com>
2016-05-18 15:22:05 -0700

    Make invoices a nested resource under user (index not working)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :bitcoin_addrs
-  resources :invoices
-  resources :users
+  resources :users do
+    resources :invoices
+  end
   root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
