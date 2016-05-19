2cddf802d4781a4ec870c30c1b190b4618baacaa
Kiko Marrs <findkiko@gmail.com>
2016-05-19 16:34:55 -0700

    Finish nesting invoices correctly

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,38 +1,37 @@
 class InvoicesController < ApplicationController
-  before_action :set_invoice, only: [:show, :edit, :update, :destroy]
+  before_action :set_user
 
   # GET /invoices
   # GET /invoices.json
   def index
-    @user = User.find(params[:user_id])
-    @invoices = @user.invoices
+    @invoices = @user.invoices.all
   end
 
   # GET /invoices/1
   # GET /invoices/1.json
   def show
+    @invoice = @user.invoices.find(params[:id])
   end
 
   # GET /invoices/new
   def new
-    @user = User.find(params[:user_id])
     @invoice = @user.invoices.new
   end
 
   # GET /invoices/1/edit
   def edit
+    @invoice = @user.invoices.find(params[:id])
   end
 
   # POST /invoices
   # POST /invoices.json
   def create
-    @user = User.find(params[:user_id])
-    @invoice = Invoice.new(invoice_params)
+    @invoice = @user.invoices.new(invoice_params)
 
     respond_to do |format|
       if @invoice.save
-        format.html { redirect_to user_invoices_path(@user,@invoice), notice: 'Invoice was successfully created.' }
-        format.json { render :show, status: :created, location: user_invoices_path(@user,@invoice) }
+        format.html { redirect_to user_invoices_path(@user, @invoice), notice: 'Invoice was successfully created.' }
+        format.json { render :show, status: :created, location: user_invoices_path(@user, @invoice) }
       else
         format.html { render :new }
         format.json { render json: @invoice.errors, status: :unprocessable_entity }
@@ -43,9 +42,10 @@ class InvoicesController < ApplicationController
   # PATCH/PUT /invoices/1
   # PATCH/PUT /invoices/1.json
   def update
+    @invoice = @user.invoices.find(params[:id])
     respond_to do |format|
       if @invoice.update(invoice_params)
-        format.html { redirect_to @invoice, notice: 'Invoice was successfully updated.' }
+        format.html { redirect_to([@user, @invoice], notice: 'Invoice was successfully updated.') }
         format.json { render :show, status: :ok, location: @invoice }
       else
         format.html { render :edit }
@@ -57,17 +57,18 @@ class InvoicesController < ApplicationController
   # DELETE /invoices/1
   # DELETE /invoices/1.json
   def destroy
+    @invoice = @user.invoices.find(params[:id])
     @invoice.destroy
     respond_to do |format|
-      format.html { redirect_to invoices_url, notice: 'Invoice was successfully destroyed.' }
+      format.html { redirect_to user_invoices_url(@user), notice: 'Invoice was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_invoice
-      @invoice = Invoice.find(params[:id])
+    def set_user
+      @user = User.find(params[:user_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
