4e6c21f3abdf84049d91509972615fc7ff6f1c09
Kiko Marrs <findkiko@gmail.com>
2016-05-19 12:34:43 -0700

    nested invoice working (new invoice still not)

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -4,7 +4,8 @@ class InvoicesController < ApplicationController
   # GET /invoices
   # GET /invoices.json
   def index
-    @invoices = Invoice.all
+    @user = User.find(params[:user_id])
+    @invoices = @user.invoices
   end
 
   # GET /invoices/1
@@ -14,7 +15,8 @@ class InvoicesController < ApplicationController
 
   # GET /invoices/new
   def new
-    @invoice = Invoice.new
+    @user = User.find(params[:user_id])
+    @invoice = @user.invoices.new
   end
 
   # GET /invoices/1/edit
@@ -24,12 +26,13 @@ class InvoicesController < ApplicationController
   # POST /invoices
   # POST /invoices.json
   def create
+    @user = User.find(params[:user_id])
     @invoice = Invoice.new(invoice_params)
 
     respond_to do |format|
       if @invoice.save
-        format.html { redirect_to @invoice, notice: 'Invoice was successfully created.' }
-        format.json { render :show, status: :created, location: @invoice }
+        format.html { redirect_to user_invoices_path(@user,@invoice), notice: 'Invoice was successfully created.' }
+        format.json { render :show, status: :created, location: user_invoices_path(@user,@invoice) }
       else
         format.html { render :new }
         format.json { render json: @invoice.errors, status: :unprocessable_entity }
