35ef3cceeb91477de5773970ea51c74fbc26bd25
Paul <paulll.lucas@gmail.com>
2016-06-16 19:47:49 -0700

    Implement ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :users do
     resources :btcinvoices, shallow: true do
       resources :bitcoin_addrs
